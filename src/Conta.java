7bbd3a430a4694730a523adae192f55d27195803
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-07-23 23:41:03 -0300

    :sparkles: feat: add class Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class Conta {
+import java.util.Objects;
+
+public class Conta {
+    private int agencia;
+    private int numero;
+    private double saldo;
+    public Conta() {
+    }
+
+    public Conta(int agencia, int numero, double saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return agencia == conta.agencia && numero == conta.numero && Double.compare(conta.saldo, saldo) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(agencia, numero, saldo);
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "agencia=" + agencia +
+                ", numero=" + numero +
+                ", saldo=" + saldo +
+                '}';
+    }
+
+    public void sacar(double valor){
+        this.saldo -= valor;
+    }
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public void transferir(double valor, Conta conta){
+        this.saldo -= valor;
+        conta.depositar(valor);
+    }
+
 }
