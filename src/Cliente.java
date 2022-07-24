1088985f53ac50c04f3403692eb96d2c28028cc9
Juliana Maria de Sousa Mesquita <julianamariasousamesquita@id.uff.br>
2022-07-23 23:56:14 -0300

    :sparkles: feat: add class Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,95 @@
-package PACKAGE_NAME;public class Cliente {
+import java.util.Objects;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String senha;
+    private Conta conta;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf, String telefone, String email, String senha, Conta conta) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+        this.conta = conta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(nome, cliente.nome) && Objects.equals(cpf, cliente.cpf) && Objects.equals(telefone, cliente.telefone) && Objects.equals(email, cliente.email) && Objects.equals(senha, cliente.senha) && Objects.equals(conta, cliente.conta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cpf, telefone, email, senha, conta);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", conta=" + conta +
+                '}';
+    }
 }
